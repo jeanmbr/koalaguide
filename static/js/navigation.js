a85e4580e420743187beff7c24962963558fb398
jeanmbr <jeanmachadobrasil@gmail.com>
2025-10-15 18:01:53 -0300

    Adding files from frontend

---
+++ static/js/navigation.js
@@ -0,0 +1,27 @@
+function redirectToHome() {
+    window.location.href = "../templates/index.html";
+}
+
+function redirectToStudy() {
+    window.location.href = "../templates/study.html";
+}
+
+function redirectToJob() {
+    window.location.href = "../templates/job.html";
+}
+
+function redirectToTourism() {
+    window.location.href = "../templates/tourism.html";
+}
+
+function redirectToAbout() {
+    window.location.href = "../templates/about.html";
+}
+
+// DEBUG
+console.log('✅ Navigation carregado! Funções disponíveis:');
+console.log('- redirectToHome()');
+console.log('- redirectToStudy()');
+console.log('- redirectToJob()');
+console.log('- redirectToTourism()');
+console.log('- redirectToAbout()');
